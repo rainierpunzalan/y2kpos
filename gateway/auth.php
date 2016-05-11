40b1a15bd9c9bd86c57136763f323aac4153d5f2
rpgpunzalan <rpgpunzalan@gmail.com>
2016-05-11 14:28:21 +0800

    auth update

--- gateway/auth.php
+++ gateway/auth.php
@@ -2,8 +2,8 @@
   include "../utils/functions.php";
   $db = new pos_functions();
   if(isset($_POST['loginFlag'])){
-    $u = $_POST['inputUsername'];
-    $p = $_POST['inputPassword'];
+    $u = $_POST['input-uname'];
+    $p = $_POST['input-pw'];
     if($db->login($u,$p)){
        $db->addHistory("",$_SESSION['y2kposusername'] . " Logged in");
       header("location: ../pages/index.php");
